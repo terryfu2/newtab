7ecefe8a500878b912b18e645f0ee71dda783604
terryfu <terryfu33@gmail.com>
2021-03-28 22:25:30 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Momentum_JS
+
+Vanilla JS version of the productivity app <Momentum> extension on Chrome (https://chrome.google.com/webstore/detail/momentum/laookkfknpbbblfpciffpaejjkokdgca)
+
+## Features
+
+- [x] Clock
+- [x] To Do List
+- [x] Weather
+- [x] Offline Support
+- [ ] Daily Quote
+- [ ] Daily Photo
+- [ ] Add Function to Load Image Faster
+
+
---
+++ js/weather.js
@@ -0,0 +1,57 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "23931b6007a2a5b3e0b9b9cf7124f6e4";
+const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    ).then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        // console.log(json)
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerHTML = `${temperature}&#8451 @${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    const coordsObj = {
+        latitude: latitude,
+        longitude: longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+    console.log("Can't access geo location");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCoords = localStorage.getItem(COORDS);
+    if (loadedCoords === null) {
+        askForCoords();
+    } else {
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
+    }
+}
+
+function init() {
+    loadCoords();
+}
+
+init();
